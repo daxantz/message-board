a71bd2b34049745b9daef76dab113b46beac52a1
Dax Davis <daxantz@hotmail.com>
2024-08-23 01:11:42 -0400

    Configured Express to use EJS as the view engine - Added route handlers for index and new routes - Set up path for views directory - Mounted routers for index and new routes

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send("Hello message board");
-});
+const indexRouter = require("./routes/index");
+const newRouter = require("./routes/new");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+app.use("/", indexRouter);
+app.use(newRouter);
 app.listen(PORT, () => {
   console.log("Message board server is running on PORT: " + PORT);
 });
